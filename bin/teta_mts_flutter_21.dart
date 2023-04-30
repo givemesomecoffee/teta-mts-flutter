a5c037e4250243468b6d615bcf3095a0267f95a4
givemesomecoffee <givemesomecoffee@yandex.ru>
2023-04-30 10:40:04 +0300

    init

---
+++ bin/teta_mts_flutter_21.dart
@@ -0,0 +1,5 @@
+import 'package:teta_mts_flutter_21/teta_mts_flutter_21.dart' as teta_mts_flutter_21;
+
+void main(List<String> arguments) {
+  print('Hello world: ${teta_mts_flutter_21.calculate()}!');
+}
---
+++ test/teta_mts_flutter_21_test.dart
@@ -0,0 +1,8 @@
+import 'package:teta_mts_flutter_21/teta_mts_flutter_21.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
