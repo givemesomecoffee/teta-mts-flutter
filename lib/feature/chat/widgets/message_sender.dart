c7a9a3d32fe935db477029dd680d55610d67ffdb
givemesomecoffee <givemesomecoffee@yandex.ru>
2023-05-28 21:41:03 +0300

    fixed_multi_instance_of_controller_in_message_sender

--- lib/feature/chat/widgets/message_sender.dart
+++ lib/feature/chat/widgets/message_sender.dart
@@ -1,33 +1,49 @@
 import 'package:chat_app/di/service_locator.dart';
 import 'package:flutter/material.dart';
 
-class MessageSender extends StatelessWidget{
+class MessageSender extends StatefulWidget {
   const MessageSender({super.key});
+
+  @override
+  State<MessageSender> createState() => _MessageSenderState();
+}
+
+class _MessageSenderState extends State<MessageSender> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final TextEditingController controller = TextEditingController();
-    return  Padding(
+    return Padding(
       padding: MediaQuery.of(context).viewInsets,
       child: Row(
         children: [
           Expanded(
             child: BottomAppBar(
               child: TextField(
-                controller: controller,
+                controller: _controller,
                 style: const TextStyle(fontSize: 16.0),
                 decoration: const InputDecoration(
                     floatingLabelBehavior: FloatingLabelBehavior.never,
                     labelText: 'Message',
                     border: OutlineInputBorder(),
-                    contentPadding: EdgeInsets.symmetric(
-                        vertical: 8.0, horizontal: 8.0)),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0)),
               ),
             ),
           ),
           IconButton(
               onPressed: () {
-                Dependencies.of(context).dbService.sendMessage(controller.text);
-                controller.text = "";
+                // TODO: есть ли смысл выносить в initState сервис с бд?
+                Dependencies.of(context)
+                    .dbService
+                    .sendMessage(_controller.text);
+                _controller.text = "";
               },
               icon: const Icon(Icons.send))
         ],
